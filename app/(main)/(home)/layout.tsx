09cd5c60dd1559edbd91d0c5e3d9c2e04c2a5fba
Pranav Tartey <pranavtartey@gmail.com>
2025-01-27 11:41:13 +0530

    fixed the most annoying bug of app not looking perfect in mobile view (app woks perfect)

--- app/(main)/(home)/layout.tsx
+++ app/(main)/(home)/layout.tsx
@@ -12,7 +12,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
       {/* <p className="z-0 text-center text-xs font-medium py-2 bg-[linear-gradient(to_right,#F89C9C,#EAD07A_38%,#78EDE3_66%,#E682EF_99%)] mix-blend-multiply">
         Witness the evolution of gaming in Jhansi
       </p> */}
-      <div className="min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
+      <div className="">
         <Navbar />
         {children}
         <Footer />
@@ -22,3 +22,4 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 };
 
 export default Layout;
+// min-h-[100dvh] grid grid-rows-[auto_1fr_auto] This class can be used when you have a very less content in your section and there is only one section and your want the looter to pushed at the end of the page but make it visible at the same time. Currently we have content already so we should not use this as it will create problems in your content flow
